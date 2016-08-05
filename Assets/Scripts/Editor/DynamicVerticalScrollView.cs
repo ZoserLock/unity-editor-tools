78a07d5e344a87754040495233ebac823fb08c39
Pablo Riedemann <zoserx@gmail.com>
2016-08-04 20:29:50 -0400

    + Initial Commit + Added Dynamic Vertical Scroll View

---
+++ Assets/Scripts/Editor/DynamicVerticalScrollView.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DynamicVerticalScrollView
+{
+    private Vector2 _scrollSize;
+    private Rect _scrollRect;
+    private Rect _elementRect;
+
+    private int _visibleHeight;
+    private int _elementSize;
+
+    private int _visibleItems;
+    private int _firstVisibleItem;
+    private int _lastVisibleItem;
+
+    private int _startSpace;
+    private int _endSpace;
+
+    private Editor _editor;
+
+    public DynamicVerticalScrollView(Editor parentEditor)
+    {
+        _editor = parentEditor;
+    }
+
+    public void RenderList<T>(List<T> elements,System.Action<T,int> callback)
+    {
+        if(callback == null || elements.Count==0 || _editor == null)
+        {
+            return;
+        }
+
+        _scrollSize = EditorGUILayout.BeginScrollView(_scrollSize);
+
+        if (Event.current.type == EventType.Layout)
+        {
+            _visibleHeight = Mathf.CeilToInt(_scrollRect.height);
+            _elementSize   = Mathf.CeilToInt(_elementRect.height);
+
+            if(_elementSize == 0 || _visibleHeight == 0)
+            {
+                _editor.Repaint();
+                _elementSize = 20;
+            }
+
+            _visibleItems = Mathf.CeilToInt(_visibleHeight / (float)_elementSize);
+
+            _firstVisibleItem = Mathf.FloorToInt(_scrollSize.y / (float)_elementSize);
+            _lastVisibleItem = Mathf.Min(elements.Count, _firstVisibleItem + _visibleItems);
+
+            _firstVisibleItem = Mathf.Min(_firstVisibleItem, _lastVisibleItem - _visibleItems);
+
+            _startSpace = _firstVisibleItem * _elementSize;
+            _endSpace = (elements.Count - _lastVisibleItem) * _elementSize;
+        }
+
+        GUILayout.Space(_startSpace);
+
+        for (int a = _firstVisibleItem; a < _lastVisibleItem; ++a)
+        {
+            callback(elements[a],a);
+        }
+
+        if(_firstVisibleItem!= _lastVisibleItem)
+        {
+            if (Event.current.type == EventType.Repaint)
+            {
+                _elementRect = GUILayoutUtility.GetLastRect();
+            }
+        }
+
+        GUILayout.Space(_endSpace);
+        EditorGUILayout.EndScrollView();
+        if (Event.current.type == EventType.Repaint)
+        {
+            _scrollRect = GUILayoutUtility.GetLastRect();
+        }
+    }
+}
---
+++ Assets/Scripts/Editor/ScrollTesterEditor.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+using System.Collections;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(ScrollTester))]
+public class ScrollTesterEditor : Editor
+{
+
+    Vector2 _scrollSize;
+
+    List<string> _buttonNames = new List<string>();
+
+    bool _scrollSystemActivated = true;
+
+    DynamicVerticalScrollView _dynamicVerticalScrollView;
+
+    void OnEnable()
+    {
+        _dynamicVerticalScrollView = new DynamicVerticalScrollView(this);
+
+        _buttonNames.Clear();
+        for (int a = 0 ;a< 5000 ;++a)
+        {
+            _buttonNames.Add("Button: "+a);
+        }
+    }
+
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        Color old = GUI.color;
+
+        if(_scrollSystemActivated)
+        {
+            GUI.color = Color.green;
+            if (GUILayout.Button("Toggle Scroll System [ENABLED]"))
+            {
+                _scrollSystemActivated = !_scrollSystemActivated;
+            }
+        }
+        else
+        {
+            GUI.color = Color.red;
+            if (GUILayout.Button("Toggle Scroll System [DISABLED]"))
+            {
+                _scrollSystemActivated = !_scrollSystemActivated;
+            }
+        }
+
+        GUI.color = old;
+
+        if (_scrollSystemActivated)
+        {
+            _dynamicVerticalScrollView.RenderList(_buttonNames, (element, index) =>
+            {
+                GUILayout.BeginHorizontal();
+                GUILayout.Button(element);
+                GUILayout.Button("X",GUILayout.ExpandWidth(false));
+                GUILayout.EndVertical();
+            });
+        }
+        else
+        {
+            _scrollSize = EditorGUILayout.BeginScrollView(_scrollSize);
+
+            for (int a = 0; a < _buttonNames.Count; ++a)
+            {
+                GUILayout.BeginHorizontal();
+                GUILayout.Button(_buttonNames[a]);
+                GUILayout.Button("X", GUILayout.ExpandWidth(false));
+                GUILayout.EndVertical();
+            }
+            EditorGUILayout.EndScrollView();
+        }
+    }
+}
